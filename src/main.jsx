596d8939a620eba8147c2e1d74c76c704f6985f9
JoaldoSanth <joaldosant3@gmail.com>
2024-05-19 02:39:39 -0300

    projeto no react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Busca_de_filmes from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Busca_de_filmes />
+  </React.StrictMode>,
+)
